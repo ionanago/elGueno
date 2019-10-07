9c1a9692ab18c08da883ddaa84d4fc9fc06daa13
ionanago <51776373+ionanago@users.noreply.github.com>
2019-10-07 14:10:14 +0200

    --jvtyd

---
+++ restServer/EjemploConSpring/src/main/java/com/ipartek/formacion/EjemploConSpring/EjemploConSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.EjemploConSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemploConSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemploConSpringApplication.class, args);
+	}
+
+}
