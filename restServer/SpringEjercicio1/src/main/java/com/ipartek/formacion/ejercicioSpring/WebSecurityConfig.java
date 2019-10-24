5f776b89785892119df0cc0e2f7cd3af4e23e76f
ionanago <51776373+ionanago@users.noreply.github.com>
2019-10-24 14:31:32 +0200

    -->

--- restServer/SpringEjercicio1/src/main/java/com/ipartek/formacion/ejercicioSpring/WebSecurityConfig.java
+++ restServer/SpringEjercicio1/src/main/java/com/ipartek/formacion/ejercicioSpring/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+    	http.authorizeRequests().antMatchers("/css/**", "/js/**", "/images/**").permitAll();
         http//.authorizeRequests().anyRequest().permitAll();
             .authorizeRequests()
                 .antMatchers("/", "/home").permitAll()
