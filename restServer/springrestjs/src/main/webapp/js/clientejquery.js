e2db87fb5e1dde6083f098f25d06017bcc4a4c71
ionanago <51776373+ionanago@users.noreply.github.com>
2019-11-05 14:18:08 +0100

    --->restJS

---
+++ restServer/springrestjs/src/main/webapp/js/clientejquery.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var URLPOST = 'http://localhost:8080/api/productos/';
+var URLDELETE = 'http://localhost:8080/api/productos/';
+var URLGET = 'http://localhost:8080/api/productos/';
+
+
+jQuery(function($) { //Equivalente a window.onload
+	$('button').click(function(e) {
+		e.preventDefault(); //Cancelar la emisión del formulario por el action
+
+		var nombre = $('#nombre').val();
+		var descripcion = $('#descripcion').val();
+		var precio = $('#precio').val();
+
+		var producto = { nombre, descripcion, precio };
+
+		console.log(producto);
+
+		ajaxRest($, URLPOST, 'POST', producto).done(function(respuesta) {
+			console.log(respuesta);
+			addProductoAListado($, producto);
+		}).fail(function(jqXHR, textStatus, errorThrown) {
+			alert(jqXHR.responseJSON.message);
+			console.log(jqXHR, textStatus, errorThrown);
+		});
+	});
+
+	listarProductos($);
+});
+
+function ajaxRest($, url, metodo, producto,id) {
+	return $.ajax({
+		url: url+id,
+		method: metodo,
+		data: JSON.stringify(producto),
+		contentType: 'application/json',
+		dataType: 'json'
+	});
+}
+
+function listarProductos($) {
+	$('#productos').empty();
+
+	$.getJSON(URL, function(respuesta) {
+		var productos = respuesta._embedded.productos;
+		console.log(productos);
+		$(productos).each(function() {
+			console.log(this);
+			addProductoAListado($, this);
+		});
+	});
+}
+
+function addProductoAListado($, producto) {
+	$('#productos').append($('<li>').html(
+		producto.nombre + ' ' + producto.descripcion + ' ' + producto.precio));
+}
